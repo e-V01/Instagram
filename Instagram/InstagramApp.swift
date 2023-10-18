ea02a82c4fa9bc135e69bc59c6162a76c6052d4c
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-18 15:25:18 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Y K on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
