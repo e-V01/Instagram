b41a8ecdaa05f83daed9db34db09de19ec180a9c
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-18 21:39:12 +0300

    last updates

---
+++ Instagram/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  Instagram
+//
+//  Created by Y K on 18.10.2023.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
