938476d37355b02fee7acfbe4de871380a0cc28d
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-18 16:19:58 +0300

    assets added, profiel view in progress

---
+++ Instagram/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Instagram
+//
+//  Created by Y K on 18.10.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
