2c066d2d935c42f5595b6620c0d6cd2820801189
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-19 13:35:36 +0300

    FeedView added

---
+++ Instagram/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Instagram
+//
+//  Created by Y K on 19.10.2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
