7b158d1523fc118898c168bb39c148b6ace14b1e
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-19 13:36:20 +0300

    2 screens added

--- Instagram/Core/Feed/FeedCell.swift
+++ Instagram/Core/Feed/FeedCell.swift
@@ -9,7 +9,81 @@ import SwiftUI
 
 struct FeedCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Image + user name
+            HStack {
+                Image("batman")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("Batman")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            // post image
+            Image("batman2")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            //action buttons
+            HStack(spacing: 16) {
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Comment on post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("share post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                Spacer()
+                
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
+            
+            //likes label
+            Text("250 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            //caption label
+            HStack {
+                Text("batman").fontWeight(.semibold) +
+                Text("This is some test caption for now")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+
+            Text("6 hours ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundColor(.gray)
+        }
     }
 }
 
