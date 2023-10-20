0969f5870f5f540fded33e90f4044b0ada6d1026
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-20 13:57:24 +0300

    added search bar view, login view in progress

---
+++ Instagram/Core/Search/SearchView.swift
@@ -0,0 +1,47 @@
+//
+//  SearchView.swift
+//  Instagram
+//
+//  Created by Y K on 20.10.2023.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText = ""
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...15, id: \.self) { user in
+                        HStack {
+                            Image("batman2")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("Batman")
+                                    .fontWeight(.semibold)
+                                Text("Batman")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
