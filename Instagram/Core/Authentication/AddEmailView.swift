2aa96ddb20a3b99342914dc484196f4b65cdae08
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-20 14:40:13 +0300

    refactored textfield with a custom .modifier for easier read, and implementation farther

---
+++ Instagram/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,61 @@
+//
+//  AddEmailView.swift
+//  Instagram
+//
+//  Created by Y K on 20.10.2023.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    @State private var email = ""
+    @Environment (\.dismiss) var dismiss
+    // to dimiss screen 
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Add your email")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Text("You'll use this email to sign in to your account")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center) // incase it is smaller screen(phone)
+                .padding(.horizontal, 24)
+            
+            TextField("Email", text: $email)
+                .autocapitalization(.none)
+                .modifier(IGTextFieldModifier())
+            
+            Button {
+                print("Next")
+            } label: {
+                Text("Next")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
---
+++ Instagram/Core/Components/IGTextFieldModifier.swift
@@ -0,0 +1,19 @@
+//
+//  IGTextFieldModifier.swift
+//  Instagram
+//
+//  Created by Y K on 20.10.2023.
+//
+
+import SwiftUI
+
+struct IGTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+}
