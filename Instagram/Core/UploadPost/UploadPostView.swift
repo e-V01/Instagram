8563b0a4348858a8929ef9003efc483240479776
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-21 13:45:10 +0300

    imported PhotosUI library, States added to control photos and choose among them

---
+++ Instagram/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,63 @@
+//
+//  UploadPostView.swift
+//  Instagram
+//
+//  Created by Y K on 21.10.2023.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct UploadPostView: View {
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @State private var photoItem: PhotosPickerItem?
+    
+    var body: some View {
+        VStack {
+            //actio toolbar
+            HStack {
+                Button {
+                  print("Cancel Upload")
+                } label: {
+                    Text("Cancel")
+                }
+                
+                Spacer()
+                
+                Text("New Post")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Button {
+                  print("Upload")
+                } label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                }
+                
+            }
+            .padding(.horizontal)
+            // post iamge an dcaption
+            HStack(spacing: 8) {
+                Image("batman")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    
+                
+                TextField("Enter your caption...", text: $caption, axis: .vertical)
+            }
+            .padding()
+            Spacer()
+        }
+        .onAppear {
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $photoItem)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
