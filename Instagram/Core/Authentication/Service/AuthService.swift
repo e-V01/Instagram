bb4fe93e8f38bea18a0335da01c89996b1295458
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-26 18:02:42 +0300

    imported FIrebase, added uploadUserData method to keep data at firestore

--- Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -34,6 +36,9 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            print("DEBUG: did create user...")
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
+        print("DEBUG: did upload user data...")
         } catch {
             print("DEBUG: failed to register user with error \(error.localizedDescription)")
             
@@ -47,5 +52,13 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        // private since we won`t need to use else wehre beyond that file
+        // async only because upload, so no throws needed to handle errors
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return}
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
